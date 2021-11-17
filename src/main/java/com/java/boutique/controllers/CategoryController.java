147783a0d2382e5f4269a1baa84a6c78c070a003
Oceane-LEGOFF <oceanekegoff@gmail.com>
2021-11-17 14:12:52 +0100

    ajout dependences + modif controller

--- src/main/java/com/java/boutique/controllers/CategoryController.java
+++ src/main/java/com/java/boutique/controllers/CategoryController.java
@@ -1,33 +1,27 @@
 package com.java.boutique.controllers;
 
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
 import com.java.boutique.dao.CategoryDao;
 import com.java.boutique.models.Category;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@Controller
+@RestController
 public class CategoryController {
 
     @Autowired
     private CategoryDao categoryDao;
 
-    @RequestMapping("/category")
+    @GetMapping("/category")
     public String index(Model model){
         model.addAttribute("listCategory", categoryDao.listAll());
         return "index";
     }
 
-    @RequestMapping("/category/{id}")
+    @GetMapping("/category/{id}")
     public @ResponseBody List<Category> findById(@PathVariable int id) {
         return categoryDao.findById(id);
     }
-
 
 }
--- src/main/java/com/java/boutique/controllers/ProductController.java
+++ src/main/java/com/java/boutique/controllers/ProductController.java
@@ -1,36 +1,32 @@
 package com.java.boutique.controllers;
 
-import com.java.boutique.dao.CategoryDao;
 import com.java.boutique.dao.ProductDao;
 import com.java.boutique.models.Product;
 import com.java.boutique.models.viewmodels.ProductViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ProductController {
-
+    
     @Autowired
     private ProductDao productService;
     private String errorMessage;
 
-    @RequestMapping("/product")
+    @GetMapping("/product")
     public String index(Model model){
         model.addAttribute("listProduct", productService.listAll());
         return "indexProduct";
     }
 
-    @RequestMapping(value = { "/products/add" }, method = RequestMethod.GET)
+    @GetMapping(value = { "/products/add" })
     public String add(Model model){
         model.addAttribute("productForm", new ProductViewModel());
         return "add";
     }
 
-    @RequestMapping(value = { "/products/add" }, method = RequestMethod.POST)
+    @PostMapping(value = { "/products/add" })
     public String addPost(Model model, @ModelAttribute("productForm") ProductViewModel productViewModel){
 
         if (productViewModel.getName() != null && productViewModel.getName().length() > 0 //
@@ -46,7 +42,6 @@ public class ProductController {
         errorMessage = "Nom et type obligatoiregi";
         model.addAttribute("errorMessage", errorMessage);
         return "add";
-
-
     }
+
 }
