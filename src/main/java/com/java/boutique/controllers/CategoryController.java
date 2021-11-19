9b529050c0d74c196763859dbb0e116117d9bd0f
ClaireMathiron <claira.m@live.fr>
2021-11-20 00:17:16 +0100

    Swagger documentation added

--- src/main/java/com/java/boutique/controllers/CategoryController.java
+++ src/main/java/com/java/boutique/controllers/CategoryController.java
@@ -2,10 +2,13 @@ package com.java.boutique.controllers;
 
 import com.java.boutique.dao.CategoryDao;
 import com.java.boutique.models.Category;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Api(description = "Categories management")
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
@@ -31,11 +34,14 @@ public class CategoryController {
      *
      * @return the list
      */
+    @ApiOperation(value = "Retrieves the whole list of categories.")
+
     public @ResponseBody List<Category> listAll(){
         return categoryDao.listAll();
     }
 
     //pagination
+    @ApiOperation(value = "If no parameter is given, retrieves the whole list, otherwise returns the elements within the given range.")
     @GetMapping("")
     public List<Category> getRange(@RequestParam(value = "range", required = false) String range) {
         if (range == null) {
@@ -51,6 +57,7 @@ public class CategoryController {
      * @params none
      * @return first category
      */
+    @ApiOperation(value = "Retrieves the first category of the list")
     @GetMapping("/first")
     public @ResponseBody List<Category> first() {
         return categoryDao.first();
@@ -62,6 +69,7 @@ public class CategoryController {
      * @params none
      * @return last category
      */
+    @ApiOperation(value = "Retrieves the last category of the list")
     @GetMapping("/last")
     public @ResponseBody List<Category> last() {
         return categoryDao.last();
@@ -73,6 +81,7 @@ public class CategoryController {
      * @params none
      * @return int 1
      */
+    @ApiOperation(value = "Adds a new category")
     @PostMapping("")
     public int add(@RequestBody Category category){
         return categoryDao.add(category);
@@ -84,6 +93,7 @@ public class CategoryController {
      * @params id
      * @return updated category
      */
+    @ApiOperation(value = "Updates a category via its id.")
     @PutMapping("/{id}")
     public int update(@RequestBody Category category, @PathVariable int id){
         return categoryDao.updateById(category, id);
@@ -95,6 +105,7 @@ public class CategoryController {
      * @params id
      * @return
      */
+    @ApiOperation(value = "Deletes a category via its id")
     @DeleteMapping("/{id}")
     public int deleteById(@PathVariable int id) {
         return categoryDao.deleteById(id);
@@ -105,6 +116,7 @@ public class CategoryController {
      * @param name
      * @return search by name
      */
+    @ApiOperation(value = "Searches for a category via its name")
     @GetMapping("/search{name}")
     public @ResponseBody List<Category> find(@RequestParam String name){
         return categoryDao.find(name);
@@ -116,12 +128,13 @@ public class CategoryController {
      * @params id
      * @return category category
      */
+    @ApiOperation(value = "Retrieves a category via its id")
     @GetMapping("/{id}")
     public @ResponseBody List<Category> findById(@PathVariable int id) {
         return categoryDao.findById(id);
     }
 
-
+    @ApiOperation(value = "Retrieves the whole list of categories with an ascending sort according to the parameter, otherwise, retrieves all elements if no parameter is given.")
     @GetMapping("/triCroissant")
     public List<Category> triCroissant(@RequestParam(value = "id", required = false) String id,
                                         @RequestParam(value = "name", required = false) String name) {
@@ -134,6 +147,7 @@ public class CategoryController {
         }
     }
 
+    @ApiOperation(value = "Retrieves the whole list of categories with a descending sort according to the parameter, otherwise, retrieves all elements if no parameter is given.")
     @GetMapping("/triDecroissant")
     public List<Category> triDecroissant(@RequestParam(value = "id", required = false) String id,
                                         @RequestParam(value = "name", required = false) String name) {
--- src/main/java/com/java/boutique/controllers/ProductController.java
+++ src/main/java/com/java/boutique/controllers/ProductController.java
@@ -2,10 +2,13 @@ package com.java.boutique.controllers;
 
 import com.java.boutique.dao.ProductDao;
 import com.java.boutique.models.Product;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Api(description = "Products management")
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -15,6 +18,7 @@ public class ProductController {
     private String errorMessage;
 
     //get all si param=0 et pagination
+    @ApiOperation(value = "Retrieves all products if no parameter is given, otherwise retrieves elements in range given.")
     @GetMapping("")
     public List<Product> getRange(@RequestParam(value = "range", required = false) String range) {
         if (range == null) {
@@ -25,41 +29,48 @@ public class ProductController {
     }
 
     //route get/id
+    @ApiOperation(value = "Retrieves a product via its id")
     @GetMapping("/{id}")
     public @ResponseBody List<Product> findById(@PathVariable int id) {
         return productDao.findById(id);
     }
 
     //route get/first
+    @ApiOperation(value = "Retrieves the first product of the list")
     @GetMapping("/first")
     public @ResponseBody List<Product> first() {
         return productDao.first();
     }
 
     //route get/last
+    @ApiOperation(value = "Retrieves the last product of the list")
     @GetMapping("/last")
     public @ResponseBody List<Product> last() {
         return productDao.last();
     }
 
     //route add
+    @ApiOperation(value = "Adds a new product")
     @PostMapping("")
     public int add(@RequestBody Product product){
         return productDao.add(product);
     }
 
     //route update
+    @ApiOperation(value = "Edits a product via its id")
     @PutMapping("/{id}")
     public int update(@RequestBody Product product, @PathVariable int id){
         return productDao.updateById(product, id);
     }
 
     //route DELETE
+    @ApiOperation(value = "Deletes a product")
     @DeleteMapping("/{id}")
     public int deleteById(@PathVariable int id) {
         return productDao.deleteById(id);
     }
 
+    @ApiOperation(value = "Searches for the products linked to the parameter given in the URL")
     @GetMapping("/search{type, rating, name, createdAt}")
     public @ResponseBody List<Product> search(
             @RequestParam (required = false) String type,
@@ -69,6 +80,7 @@ public class ProductController {
         return productDao.search(type, name, createdAt);
     }
 
+    @ApiOperation(value = "Retrieves the whole list with an ascending sort according to the parameter, otherwise, retrieves all elements if no parameter is given.")
     @GetMapping("/triCroissant")
     public List<Product> triCroissant(@RequestParam(value = "id", required = false) String id,
                                       @RequestParam(value = "type", required = false) String type,
@@ -83,6 +95,8 @@ public class ProductController {
             return productDao.listAll();
         }
     }
+
+    @ApiOperation(value = "Retrieves the whole list with a descending sort according to the parameter, otherwise, retrieves all elements if no parameter is given.")
     @GetMapping("/triDecroissant")
     public List<Product> triDecroissant(@RequestParam(value = "id", required = false) String id,
                                       @RequestParam(value = "type", required = false) String type,
